386411e5c952dbd57a738caff7bf41067d203ce9
Sergey <me@serge.xyz>
2017-11-18 02:07:11 +0300

    p:80

--- app.js
+++ app.js
@@ -36,6 +36,6 @@ app.ws('/', (ws, req) => {
   });
 });
 
-app.listen(3000,  () => {
-  console.log('Example app listening on port 3000!');
+app.listen(80,  () => {
+  console.log('Example app listening on port 80!');
 });
