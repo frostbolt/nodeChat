b32187b3cbea3093e6f72153e45a7a885dacf772
Sergey <me@serge.xyz>
2017-11-18 01:59:01 +0300

    init

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var app = express();
+var expressWs = require('express-ws')(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static('public'));
+ 
+var clientUsernames = {};
+
+function dispatch(clients, msg) {
+	console.log(msg);
+	clients.forEach((e, i, a) => {
+		e.send(msg);
+	});
+}
+
+app.ws('/', (ws, req) => {
+  ws.on('message', (msg) => {
+    let data = JSON.parse(msg);
+    let clients = expressWs.getWss().clients;
+    if (data["message"] != undefined) {
+    	dispatch(clients, '' + data["user"] + ": " + data["message"]);
+    } else {
+    	dispatch(clients, '### '  + data["user"] + '   joins! ###');
+    	clientUsernames[ws] = data["user"];
+    }
+  });
+  ws.on('close', () => {
+  	let clients = expressWs.getWss().clients;
+  	dispatch(clients, "### " + clientUsernames[ws] + " left! ###")
+  });
+});
+
+app.listen(3000,  () => {
+  console.log('Example app listening on port 3000!');
+});
---
+++ public/javascripts/main.js
@@ -0,0 +1,43 @@
+console.log("main.js loaded!");
+
+
+var login_div = document.querySelector("#login");
+var chat_div = document.querySelector("#chat");
+
+var btn_send = document.querySelector("#btn_send");
+var btn_login = document.querySelector("#btn_login");
+var login_text = document.querySelector("#login_text");
+var textarea = document.querySelector("#chat_text");
+var chat_msg = document.querySelector("#chat_msg");
+
+var ws = new WebSocket(`ws://localhost:3000`);
+
+var userName = "unknown"
+
+ws.onerror = () => console.log('WebSocket error');
+ws.onopen = () => console.log('WebSocket connection established');
+ws.onclose = () => console.log('WebSocket connection closed');
+ws.onmessage = (message) => {
+	console.log('Server sent:', message);
+	textarea.innerHTML += '\n' + message.data;
+}
+
+btn_login.onclick = () => {
+	userName = (login_text.value == "") ? "unknown" : login_text.value ;
+	login_div.classList.add("hidden");
+	chat_div.classList.remove("hidden");
+	ws.send(JSON.stringify({
+		user: userName
+	}));
+}
+
+btn_send.onclick = () => {
+	console.log("btn clicked");
+
+	ws.send(JSON.stringify({
+		user: userName,
+		message:  chat_msg.value
+	}));
+
+}
+
